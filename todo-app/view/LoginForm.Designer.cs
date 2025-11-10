15519d21a953b892419fbaa97ceb5e1ed3f8fcae
Luu Dinh The Tai <luudinthetai741@gmail.com>
2025-11-10 14:56:49 +0700

    refactor: 6 update login, register form UI

--- todo-app/view/LoginForm.Designer.cs
+++ todo-app/view/LoginForm.Designer.cs
@@ -31,40 +31,35 @@ partial class LoginForm
     /// </summary>
     private void InitializeComponent()
     {
-        gBLogin = new System.Windows.Forms.GroupBox();
-        btnRegister = new System.Windows.Forms.Button();
-        lblOr = new System.Windows.Forms.Label();
-        btnLogin = new System.Windows.Forms.Button();
-        tBPassword = new System.Windows.Forms.TextBox();
-        lblPassword = new System.Windows.Forms.Label();
-        tBUsername = new System.Windows.Forms.TextBox();
-        lblUsername = new System.Windows.Forms.Label();
-        gBLogin.SuspendLayout();
+        ComponentResourceManager resources = new ComponentResourceManager(typeof(LoginForm));
+        btnRegister = new Button();
+        lblOr = new Label();
+        btnLogin = new Button();
+        tBPassword = new TextBox();
+        lblPassword = new Label();
+        tBUsername = new TextBox();
+        lblUsername = new Label();
+        panel1 = new Panel();
+        panel3 = new Panel();
+        pictureBox1 = new PictureBox();
+        panel2 = new Panel();
+        label1 = new Label();
+        label2 = new Label();
+        label3 = new Label();
+        label4 = new Label();
+        panel1.SuspendLayout();
+        panel3.SuspendLayout();
+        ((ISupportInitialize)pictureBox1).BeginInit();
+        panel2.SuspendLayout();
         SuspendLayout();
-        // 
-        // gBLogin
-        // 
-        gBLogin.Controls.Add(btnRegister);
-        gBLogin.Controls.Add(lblOr);
-        gBLogin.Controls.Add(btnLogin);
-        gBLogin.Controls.Add(tBPassword);
-        gBLogin.Controls.Add(lblPassword);
-        gBLogin.Controls.Add(tBUsername);
-        gBLogin.Controls.Add(lblUsername);
-        gBLogin.Dock = System.Windows.Forms.DockStyle.Fill;
-        gBLogin.Font = new System.Drawing.Font("Segoe UI", 16F);
-        gBLogin.Location = new System.Drawing.Point(0, 0);
-        gBLogin.Name = "gBLogin";
-        gBLogin.Size = new System.Drawing.Size(800, 450);
-        gBLogin.TabIndex = 0;
-        gBLogin.TabStop = false;
-        gBLogin.Text = "Đăng nhập";
         // 
         // btnRegister
         // 
-        btnRegister.Location = new System.Drawing.Point(257, 334);
+        btnRegister.FlatStyle = FlatStyle.Flat;
+        btnRegister.Font = new Font("Segoe UI", 13F);
+        btnRegister.Location = new Point(12, 308);
         btnRegister.Name = "btnRegister";
-        btnRegister.Size = new System.Drawing.Size(263, 44);
+        btnRegister.Size = new Size(263, 44);
         btnRegister.TabIndex = 6;
         btnRegister.Text = "Đăng ký";
         btnRegister.UseVisualStyleBackColor = true;
@@ -72,82 +67,191 @@ partial class LoginForm
         // 
         // lblOr
         // 
-        lblOr.Font = new System.Drawing.Font("Segoe UI", 13F);
-        lblOr.Location = new System.Drawing.Point(257, 304);
+        lblOr.Font = new Font("Segoe UI", 11F);
+        lblOr.Location = new Point(12, 278);
         lblOr.Name = "lblOr";
-        lblOr.Size = new System.Drawing.Size(263, 27);
+        lblOr.Size = new Size(263, 27);
         lblOr.TabIndex = 5;
         lblOr.Text = "hoặc";
-        lblOr.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+        lblOr.TextAlign = ContentAlignment.TopCenter;
         // 
         // btnLogin
         // 
-        btnLogin.Location = new System.Drawing.Point(257, 257);
+        btnLogin.BackColor = SystemColors.MenuHighlight;
+        btnLogin.FlatStyle = FlatStyle.Flat;
+        btnLogin.Font = new Font("Segoe UI", 13F, FontStyle.Bold);
+        btnLogin.ForeColor = SystemColors.Window;
+        btnLogin.Location = new Point(12, 231);
         btnLogin.Name = "btnLogin";
-        btnLogin.Size = new System.Drawing.Size(263, 44);
+        btnLogin.Size = new Size(263, 44);
         btnLogin.TabIndex = 4;
         btnLogin.Text = "Đăng nhập";
-        btnLogin.UseVisualStyleBackColor = true;
+        btnLogin.UseVisualStyleBackColor = false;
         btnLogin.Click += btnLogin_Click;
         // 
         // tBPassword
         // 
-        tBPassword.Location = new System.Drawing.Point(257, 215);
+        tBPassword.Font = new Font("Segoe UI", 13F);
+        tBPassword.Location = new Point(12, 194);
         tBPassword.Name = "tBPassword";
-        tBPassword.Size = new System.Drawing.Size(263, 36);
+        tBPassword.Size = new Size(263, 31);
         tBPassword.TabIndex = 3;
         tBPassword.UseSystemPasswordChar = true;
         // 
         // lblPassword
         // 
-        lblPassword.Font = new System.Drawing.Font("Segoe UI", 13F);
-        lblPassword.Location = new System.Drawing.Point(257, 185);
+        lblPassword.Font = new Font("Segoe UI", 13F);
+        lblPassword.Location = new Point(12, 164);
         lblPassword.Name = "lblPassword";
-        lblPassword.Size = new System.Drawing.Size(263, 27);
+        lblPassword.Size = new Size(263, 27);
         lblPassword.TabIndex = 2;
         lblPassword.Text = "Mật khẩu";
         // 
         // tBUsername
         // 
-        tBUsername.ForeColor = System.Drawing.SystemColors.WindowText;
-        tBUsername.Location = new System.Drawing.Point(257, 146);
+        tBUsername.Font = new Font("Segoe UI", 13F);
+        tBUsername.ForeColor = SystemColors.WindowText;
+        tBUsername.Location = new Point(12, 130);
         tBUsername.Name = "tBUsername";
-        tBUsername.Size = new System.Drawing.Size(263, 36);
+        tBUsername.Size = new Size(263, 31);
         tBUsername.TabIndex = 1;
         // 
         // lblUsername
         // 
-        lblUsername.Font = new System.Drawing.Font("Segoe UI", 13F);
-        lblUsername.Location = new System.Drawing.Point(257, 116);
+        lblUsername.Font = new Font("Segoe UI", 13F);
+        lblUsername.Location = new Point(12, 100);
         lblUsername.Name = "lblUsername";
-        lblUsername.Size = new System.Drawing.Size(263, 27);
+        lblUsername.Size = new Size(263, 27);
         lblUsername.TabIndex = 0;
         lblUsername.Text = "Tài khoản";
         // 
+        // panel1
+        // 
+        panel1.Controls.Add(panel3);
+        panel1.Controls.Add(panel2);
+        panel1.Dock = DockStyle.Fill;
+        panel1.Location = new Point(0, 0);
+        panel1.Name = "panel1";
+        panel1.Size = new Size(800, 450);
+        panel1.TabIndex = 1;
+        // 
+        // panel3
+        // 
+        panel3.BackColor = SystemColors.Window;
+        panel3.Controls.Add(label4);
+        panel3.Controls.Add(label3);
+        panel3.Controls.Add(label2);
+        panel3.Controls.Add(pictureBox1);
+        panel3.Dock = DockStyle.Fill;
+        panel3.Location = new Point(287, 0);
+        panel3.Name = "panel3";
+        panel3.Size = new Size(513, 450);
+        panel3.TabIndex = 9;
+        // 
+        // pictureBox1
+        // 
+        pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+        pictureBox1.Location = new Point(6, 130);
+        pictureBox1.Name = "pictureBox1";
+        pictureBox1.Size = new Size(200, 200);
+        pictureBox1.TabIndex = 0;
+        pictureBox1.TabStop = false;
+        // 
+        // panel2
+        // 
+        panel2.Controls.Add(btnLogin);
+        panel2.Controls.Add(label1);
+        panel2.Controls.Add(lblPassword);
+        panel2.Controls.Add(btnRegister);
+        panel2.Controls.Add(tBPassword);
+        panel2.Controls.Add(lblOr);
+        panel2.Controls.Add(tBUsername);
+        panel2.Controls.Add(lblUsername);
+        panel2.Dock = DockStyle.Left;
+        panel2.Location = new Point(0, 0);
+        panel2.Name = "panel2";
+        panel2.Size = new Size(287, 450);
+        panel2.TabIndex = 8;
+        // 
+        // label1
+        // 
+        label1.AutoSize = true;
+        label1.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
+        label1.Location = new Point(12, 70);
+        label1.Name = "label1";
+        label1.Size = new Size(126, 30);
+        label1.TabIndex = 7;
+        label1.Text = "Đăng nhập";
+        // 
+        // label2
+        // 
+        label2.AutoSize = true;
+        label2.Font = new Font("Segoe UI", 22F, FontStyle.Bold);
+        label2.Location = new Point(212, 184);
+        label2.Name = "label2";
+        label2.Size = new Size(213, 41);
+        label2.TabIndex = 1;
+        label2.Text = "Task Manager";
+        // 
+        // label3
+        // 
+        label3.AutoSize = true;
+        label3.Font = new Font("Segoe UI", 13F);
+        label3.ForeColor = SystemColors.MenuHighlight;
+        label3.Location = new Point(212, 225);
+        label3.Name = "label3";
+        label3.Size = new Size(68, 25);
+        label3.TabIndex = 2;
+        label3.Text = "Tiện lợi";
+        // 
+        // label4
+        // 
+        label4.AutoSize = true;
+        label4.Font = new Font("Segoe UI", 13F);
+        label4.ForeColor = SystemColors.MenuHighlight;
+        label4.Location = new Point(212, 254);
+        label4.Name = "label4";
+        label4.Size = new Size(83, 25);
+        label4.TabIndex = 3;
+        label4.Text = "Hiệu quả";
+        // 
         // LoginForm
         // 
-        AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        ClientSize = new System.Drawing.Size(800, 450);
-        Controls.Add(gBLogin);
-        FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(800, 450);
+        Controls.Add(panel1);
+        FormBorderStyle = FormBorderStyle.FixedDialog;
         MaximizeBox = false;
         MinimizeBox = false;
-        StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+        Name = "LoginForm";
+        StartPosition = FormStartPosition.CenterScreen;
         Text = "Đăng nhập";
-        gBLogin.ResumeLayout(false);
-        gBLogin.PerformLayout();
+        panel1.ResumeLayout(false);
+        panel3.ResumeLayout(false);
+        panel3.PerformLayout();
+        ((ISupportInitialize)pictureBox1).EndInit();
+        panel2.ResumeLayout(false);
+        panel2.PerformLayout();
         ResumeLayout(false);
     }
 
     private System.Windows.Forms.Label lblPassword;
     private System.Windows.Forms.Label lblUsername;
     private System.Windows.Forms.TextBox tBUsername;
-    private System.Windows.Forms.GroupBox gBLogin;
     private System.Windows.Forms.TextBox tBPassword;
     private System.Windows.Forms.Button btnLogin;
     private System.Windows.Forms.Label lblOr;
     private System.Windows.Forms.Button btnRegister;
 
     #endregion
+
+    private Panel panel1;
+    private Label label1;
+    private Panel panel2;
+    private Panel panel3;
+    private PictureBox pictureBox1;
+    private Label label2;
+    private Label label4;
+    private Label label3;
 }
--- todo-app/view/RegisterForm.Designer.cs
+++ todo-app/view/RegisterForm.Designer.cs
@@ -31,131 +31,216 @@ partial class RegisterForm
     /// </summary>
     private void InitializeComponent()
     {
-        gBRegister = new System.Windows.Forms.GroupBox();
-        btnLogin = new System.Windows.Forms.Button();
-        label4 = new System.Windows.Forms.Label();
-        btnRegister = new System.Windows.Forms.Button();
-        tBCofirmPassword = new System.Windows.Forms.TextBox();
-        label3 = new System.Windows.Forms.Label();
-        tbPassword = new System.Windows.Forms.TextBox();
-        label2 = new System.Windows.Forms.Label();
-        tBUsername = new System.Windows.Forms.TextBox();
-        label1 = new System.Windows.Forms.Label();
-        gBRegister.SuspendLayout();
+        ComponentResourceManager resources = new ComponentResourceManager(typeof(RegisterForm));
+        btnLogin = new Button();
+        label4 = new Label();
+        btnRegister = new Button();
+        tBCofirmPassword = new TextBox();
+        label3 = new Label();
+        tbPassword = new TextBox();
+        label2 = new Label();
+        tBUsername = new TextBox();
+        label1 = new Label();
+        panel1 = new Panel();
+        label5 = new Label();
+        panel2 = new Panel();
+        pictureBox1 = new PictureBox();
+        label6 = new Label();
+        label7 = new Label();
+        label8 = new Label();
+        panel1.SuspendLayout();
+        panel2.SuspendLayout();
+        ((ISupportInitialize)pictureBox1).BeginInit();
         SuspendLayout();
-        // 
-        // gBRegister
-        // 
-        gBRegister.Controls.Add(btnLogin);
-        gBRegister.Controls.Add(label4);
-        gBRegister.Controls.Add(btnRegister);
-        gBRegister.Controls.Add(tBCofirmPassword);
-        gBRegister.Controls.Add(label3);
-        gBRegister.Controls.Add(tbPassword);
-        gBRegister.Controls.Add(label2);
-        gBRegister.Controls.Add(tBUsername);
-        gBRegister.Controls.Add(label1);
-        gBRegister.Dock = System.Windows.Forms.DockStyle.Fill;
-        gBRegister.Font = new System.Drawing.Font("Segoe UI", 16F);
-        gBRegister.Location = new System.Drawing.Point(0, 0);
-        gBRegister.Name = "gBRegister";
-        gBRegister.Size = new System.Drawing.Size(800, 450);
-        gBRegister.TabIndex = 0;
-        gBRegister.TabStop = false;
-        gBRegister.Text = "Đăng ký";
         // 
         // btnLogin
         // 
-        btnLogin.Location = new System.Drawing.Point(269, 366);
+        btnLogin.FlatStyle = FlatStyle.Flat;
+        btnLogin.Font = new Font("Segoe UI", 13F);
+        btnLogin.Location = new Point(12, 334);
         btnLogin.Name = "btnLogin";
-        btnLogin.Size = new System.Drawing.Size(234, 47);
+        btnLogin.Size = new Size(263, 47);
         btnLogin.TabIndex = 8;
         btnLogin.Text = "Đăng nhập";
         btnLogin.UseVisualStyleBackColor = true;
         // 
         // label4
         // 
-        label4.Font = new System.Drawing.Font("Segoe UI", 13F);
-        label4.Location = new System.Drawing.Point(269, 335);
+        label4.Font = new Font("Segoe UI", 11F);
+        label4.Location = new Point(12, 303);
         label4.Name = "label4";
-        label4.Size = new System.Drawing.Size(234, 28);
+        label4.Size = new Size(263, 28);
         label4.TabIndex = 7;
         label4.Text = "hoặc";
-        label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+        label4.TextAlign = ContentAlignment.TopCenter;
         // 
         // btnRegister
         // 
-        btnRegister.Location = new System.Drawing.Point(269, 285);
+        btnRegister.BackColor = SystemColors.MenuHighlight;
+        btnRegister.FlatStyle = FlatStyle.Flat;
+        btnRegister.Font = new Font("Segoe UI", 13F, FontStyle.Bold);
+        btnRegister.ForeColor = SystemColors.Window;
+        btnRegister.Location = new Point(12, 253);
         btnRegister.Name = "btnRegister";
-        btnRegister.Size = new System.Drawing.Size(234, 47);
+        btnRegister.Size = new Size(263, 47);
         btnRegister.TabIndex = 6;
         btnRegister.Text = "Đăng ký";
-        btnRegister.UseVisualStyleBackColor = true;
+        btnRegister.UseVisualStyleBackColor = false;
         btnRegister.Click += btnRegister_Click;
         // 
         // tBCofirmPassword
         // 
-        tBCofirmPassword.Location = new System.Drawing.Point(269, 243);
+        tBCofirmPassword.Font = new Font("Segoe UI", 13F);
+        tBCofirmPassword.Location = new Point(12, 216);
         tBCofirmPassword.Name = "tBCofirmPassword";
-        tBCofirmPassword.Size = new System.Drawing.Size(234, 36);
+        tBCofirmPassword.Size = new Size(263, 31);
         tBCofirmPassword.TabIndex = 5;
         tBCofirmPassword.UseSystemPasswordChar = true;
         // 
         // label3
         // 
-        label3.Font = new System.Drawing.Font("Segoe UI", 13F);
-        label3.Location = new System.Drawing.Point(269, 212);
+        label3.Font = new Font("Segoe UI", 13F);
+        label3.Location = new Point(12, 185);
         label3.Name = "label3";
-        label3.Size = new System.Drawing.Size(234, 28);
+        label3.Size = new Size(263, 28);
         label3.TabIndex = 4;
         label3.Text = "Nhập lại mật khẩu";
         // 
         // tbPassword
         // 
-        tbPassword.Location = new System.Drawing.Point(269, 173);
+        tbPassword.Font = new Font("Segoe UI", 13F);
+        tbPassword.Location = new Point(12, 151);
         tbPassword.Name = "tbPassword";
-        tbPassword.Size = new System.Drawing.Size(234, 36);
+        tbPassword.Size = new Size(263, 31);
         tbPassword.TabIndex = 3;
         tbPassword.UseSystemPasswordChar = true;
         // 
         // label2
         // 
-        label2.Font = new System.Drawing.Font("Segoe UI", 13F);
-        label2.Location = new System.Drawing.Point(269, 142);
+        label2.Font = new Font("Segoe UI", 13F);
+        label2.Location = new Point(12, 120);
         label2.Name = "label2";
-        label2.Size = new System.Drawing.Size(234, 28);
+        label2.Size = new Size(263, 28);
         label2.TabIndex = 2;
         label2.Text = "Mật khẩu";
         // 
         // tBUsername
         // 
-        tBUsername.Location = new System.Drawing.Point(269, 103);
+        tBUsername.Font = new Font("Segoe UI", 13F);
+        tBUsername.Location = new Point(12, 86);
         tBUsername.Name = "tBUsername";
-        tBUsername.Size = new System.Drawing.Size(234, 36);
+        tBUsername.Size = new Size(263, 31);
         tBUsername.TabIndex = 1;
         // 
         // label1
         // 
-        label1.Font = new System.Drawing.Font("Segoe UI", 13F);
-        label1.Location = new System.Drawing.Point(269, 72);
+        label1.Font = new Font("Segoe UI", 13F);
+        label1.Location = new Point(12, 55);
         label1.Name = "label1";
-        label1.Size = new System.Drawing.Size(234, 28);
+        label1.Size = new Size(263, 28);
         label1.TabIndex = 0;
         label1.Text = "Tài khoản";
         // 
+        // panel1
+        // 
+        panel1.Controls.Add(label5);
+        panel1.Controls.Add(btnLogin);
+        panel1.Controls.Add(btnRegister);
+        panel1.Controls.Add(label4);
+        panel1.Controls.Add(label1);
+        panel1.Controls.Add(tBUsername);
+        panel1.Controls.Add(tBCofirmPassword);
+        panel1.Controls.Add(label2);
+        panel1.Controls.Add(label3);
+        panel1.Controls.Add(tbPassword);
+        panel1.Dock = DockStyle.Left;
+        panel1.Location = new Point(0, 0);
+        panel1.Name = "panel1";
+        panel1.Size = new Size(287, 450);
+        panel1.TabIndex = 1;
+        // 
+        // label5
+        // 
+        label5.AutoSize = true;
+        label5.Font = new Font("Segoe UI", 16F, FontStyle.Bold);
+        label5.Location = new Point(12, 25);
+        label5.Name = "label5";
+        label5.Size = new Size(98, 30);
+        label5.TabIndex = 9;
+        label5.Text = "Đăng ký";
+        // 
+        // panel2
+        // 
+        panel2.BackColor = SystemColors.Window;
+        panel2.Controls.Add(label6);
+        panel2.Controls.Add(label7);
+        panel2.Controls.Add(label8);
+        panel2.Controls.Add(pictureBox1);
+        panel2.Dock = DockStyle.Fill;
+        panel2.Location = new Point(287, 0);
+        panel2.Name = "panel2";
+        panel2.Size = new Size(513, 450);
+        panel2.TabIndex = 2;
+        // 
+        // pictureBox1
+        // 
+        pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+        pictureBox1.Location = new Point(6, 131);
+        pictureBox1.Name = "pictureBox1";
+        pictureBox1.Size = new Size(200, 200);
+        pictureBox1.TabIndex = 0;
+        pictureBox1.TabStop = false;
+        // 
+        // label6
+        // 
+        label6.AutoSize = true;
+        label6.Font = new Font("Segoe UI", 13F);
+        label6.ForeColor = SystemColors.MenuHighlight;
+        label6.Location = new Point(212, 255);
+        label6.Name = "label6";
+        label6.Size = new Size(83, 25);
+        label6.TabIndex = 6;
+        label6.Text = "Hiệu quả";
+        // 
+        // label7
+        // 
+        label7.AutoSize = true;
+        label7.Font = new Font("Segoe UI", 13F);
+        label7.ForeColor = SystemColors.MenuHighlight;
+        label7.Location = new Point(212, 226);
+        label7.Name = "label7";
+        label7.Size = new Size(68, 25);
+        label7.TabIndex = 5;
+        label7.Text = "Tiện lợi";
+        // 
+        // label8
+        // 
+        label8.AutoSize = true;
+        label8.Font = new Font("Segoe UI", 22F, FontStyle.Bold);
+        label8.Location = new Point(212, 185);
+        label8.Name = "label8";
+        label8.Size = new Size(213, 41);
+        label8.TabIndex = 4;
+        label8.Text = "Task Manager";
+        // 
         // RegisterForm
         // 
-        AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        ClientSize = new System.Drawing.Size(800, 450);
-        Controls.Add(gBRegister);
-        FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+        AutoScaleDimensions = new SizeF(7F, 15F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(800, 450);
+        Controls.Add(panel2);
+        Controls.Add(panel1);
+        FormBorderStyle = FormBorderStyle.FixedDialog;
         MaximizeBox = false;
         MinimizeBox = false;
-        StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+        Name = "RegisterForm";
+        StartPosition = FormStartPosition.CenterScreen;
         Text = "Đăng ký";
-        gBRegister.ResumeLayout(false);
-        gBRegister.PerformLayout();
+        panel1.ResumeLayout(false);
+        panel1.PerformLayout();
+        panel2.ResumeLayout(false);
+        panel2.PerformLayout();
+        ((ISupportInitialize)pictureBox1).EndInit();
         ResumeLayout(false);
     }
 
@@ -169,7 +254,13 @@ partial class RegisterForm
     private System.Windows.Forms.Label label4;
     private System.Windows.Forms.Button btnLogin;
 
-    private System.Windows.Forms.GroupBox gBRegister;
-
     #endregion
+
+    private Panel panel1;
+    private Label label5;
+    private Panel panel2;
+    private PictureBox pictureBox1;
+    private Label label6;
+    private Label label7;
+    private Label label8;
 }
